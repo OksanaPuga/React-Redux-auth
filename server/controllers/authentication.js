5a7f3835d02d922bc2383e310b9470fc63950171
Oksana <puga.oksana@gmail.com>
2019-02-26 00:27:00 +0200

    added jwt strategy

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
+const config = require('../config');
 const User = require('../models/user');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -23,7 +30,7 @@ exports.signup = function(req, res, next) {
         user.save(function(err) {
             if (err) return next(err);
 
-            res.send({ success: true });
+            res.send({ token: tokenForUser(user) });
         });
     });
 };
