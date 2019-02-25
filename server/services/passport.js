4c8963d5530094f901f06f99afcb032a88e70ad8
Oksana <puga.oksana@gmail.com>
2019-02-26 01:44:32 +0200

    added logal strategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,13 +1,34 @@
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const config = require('../config');
 const User = require('../models/user');
 
+const localOptions = {
+    usernameField: 'email'
+};
+
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     secretOrKey: config.secret
 };
+
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+    User.findOne({ email: email }, function(err, user) {
+        if (err) return done(err, false);
+        if (!user) {
+            return done(null, false)
+        } else {
+            user.comparePassword(password, function(err, isMatch) {
+                if (err) return done(err);
+                if (!isMatch) return done(null, false);
+
+                return done(null, user);
+            });
+        } 
+    });
+});
 
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
     User.findById(payload.sub, function(err, user) {
@@ -22,3 +43,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 passport.use(jwtLogin);
+passport.use(localLogin);
